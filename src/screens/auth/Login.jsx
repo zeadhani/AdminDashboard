c84792a693712bc4cdccd064dd478b1295317523
Zead Hani <zeadhani88@gmail.com>
2023-01-27 23:38:13 +0200

    login screen

--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -17,61 +17,13 @@ import { useState } from "react";
 import FormCard from "../../components/Forms/FormCard";
 import FormButton from "../../components/Forms/FormButton";
 import CustomTextField from "../../components/Forms/CustomTextField";
+import { mockLoginData as itemData } from "../../data/mockData";
+
 
 const initialValues = {
   email: "",
   password: "",
 };
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
-    title: "Bed",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
-    title: "Kitchen",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
-    title: "Sink",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
-    title: "Books",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
-    title: "Chairs",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
-    title: "Candle",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
-    title: "Laptop",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
-    title: "Doors",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
-    title: "Storage",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
-    title: "Coffee table",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
-    title: "Blinds",
-  },
-];
 function Login() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -89,7 +41,7 @@ function Login() {
   });
 
   return (
-    <Stack direction={"row"} height={"100vh"}>
+    <Stack direction={"row"} height={"100vh"} overflow={"hidden"}>
       <Box width={"100%"} display={"flex"} alignContent={"center"}>
         <Formik
           onSubmit={handleFormSubmit}
@@ -143,10 +95,19 @@ function Login() {
         </Formik>
       </Box>
 
-      <Box width={"100%"} bgcolor={colors.primary[600]} overflow={"scroll"}>
-        <ImageList sx={{ padding: 2 }} variant="woven" cols={3} gap={8}>
+      <Box width={"100%"} className="loginscreen">
+        <ImageList
+          sx={{
+            padding: 2,
+            overflowY: "scroll",
+            backgroundColor: colors.primary[600],
+          }}
+          variant="woven"
+          cols={3}
+          gap={8}
+        >
           {itemData.map((item) => (
-            <ImageListItem key={item.img}>
+            <ImageListItem key={item.title}>
               <img
                 src={`${item.img}?w=161&fit=crop&auto=format`}
                 srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
