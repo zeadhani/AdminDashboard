eebaeb1d5510e8a3a092b005326c054fa2a97e07
zeadhani <zeadhani88@gmail.com>
2023-04-09 21:54:28 +0200

    email

--- src/components/contactUs/ReplyContainer.jsx
+++ src/components/contactUs/ReplyContainer.jsx
@@ -13,6 +13,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { messageCountActions } from "../../store/messageCountSlice";
+import authFetch from "../../services/interceptors";
 
 const BoxButton = ({ theme, matches, onClick, children }) => {
   return (
@@ -35,6 +36,8 @@ function ReplyContainer({ message }) {
   const matches = useMediaQuery("(max-width:600px)");
   const [replyMessage, setReplyMessage] = useState("");
   const [replyMessageError, setReplyMessageError] = useState(false);
+  const [replySubject, setReplySubject] = useState("");
+  const [subjectError, setSubjectError] = useState(false);
   const count = useSelector((state) => state.Count.count);
   const dispatch = useDispatch();
   const handlereplyChange = () => {
@@ -44,12 +47,27 @@ function ReplyContainer({ message }) {
     setReplyMessageError(false);
     setReplyMessage(event.target.value);
   };
+  const handleSubjectChange = (event) => {
+    setSubjectError(false);
+    setReplySubject(event.target.value);
+  };
   const handleSendMessage = async () => {
+    if (!replySubject) {
+      setSubjectError(true);
+      return;
+    }
     if (!replyMessage) {
       setReplyMessageError(true);
       return;
     }
+
     try {
+      await authFetch.post("/contactus/reply", {
+        userEmail: message.Users.email,
+        message: replyMessage,
+        id: message.id,
+        subject:replySubject
+      });
       toast.success("Email sent");
       setReplied(true);
       setReply(false);
@@ -94,13 +112,23 @@ function ReplyContainer({ message }) {
         </>
       )}
       {reply && (
-        <Box my={2} display={"flex"} flexDirection={"column"} gap={2}>
+        <Box my={1} display={"flex"} flexDirection={"column"} gap={2}>
           <Typography variant="body2" color={"grey"}>
             Kindly input your message, as it will be sent as an email within a
             template.
           </Typography>
           <TextField
-            label="Enter your message"
+            label="Email Subject"
+            rows={2}
+            variant="outlined"
+            fullWidth
+            error={subjectError}
+            value={replySubject}
+            onChange={handleSubjectChange}
+            helperText={subjectError ? "Please write a subject" : ""}
+          />
+          <TextField
+            label="Enter your Email"
             multiline
             rows={10}
             variant="outlined"
